c47c1cd80f30c509064faba4a30fab8f92a3f8e4
Yoonho Aaron Kim <yoonho.kim@hp.com>
2019-10-28 17:03:38 +0900

    Base Navigation Drawer 적용 - Jetpack Navigation

---
+++ app/src/main/java/com/hppk/toctw/ui/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.hppk.toctw.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import com.hppk.toctw.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val navController: NavController by lazy { findNavController(R.id.nav_host_fragment) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupNavigationMenu()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.schedulesFragment,
+                R.id.stampsFragment,
+                R.id.settingsFragment
+            ), drawerLayout
+        )
+        return navController.navigateUp(appBarConfiguration)
+    }
+
+    private fun setupNavigationMenu() {
+        navView.setupWithNavController(navController)
+    }
+
+}
