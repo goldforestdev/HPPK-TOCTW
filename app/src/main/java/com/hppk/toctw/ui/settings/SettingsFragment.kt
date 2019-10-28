947171e4b1b8c76aa563d7ee8407c70e11c6610e
Yoonho Aaron Kim <yoonho.kim@hp.com>
2019-10-28 17:37:23 +0900

    Settings 추가

--- app/src/main/java/com/hppk/toctw/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/hppk/toctw/ui/settings/SettingsFragment.kt
@@ -1,25 +1,89 @@
 package com.hppk.toctw.ui.settings
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.app.ShareCompat
+import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 import com.hppk.toctw.R
 
-/**
- * A simple [Fragment] subclass.
- */
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceClickListener {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    private val prefRateReview: Preference by lazy {
+        findPreference<Preference>(getString(R.string.key_rating_review)) as Preference
+    }
+    private val prefFeedback: Preference by lazy {
+        findPreference<Preference>(getString(R.string.key_feedback)) as Preference
+    }
+    private val prefVersion: Preference by lazy {
+        findPreference<Preference>(getString(R.string.key_version)) as Preference
+    }
+    private val prefDevelopers: Preference by lazy {
+        findPreference<Preference>(getString(R.string.key_developers)) as Preference
     }
 
+    private val appVersion: String by lazy {
+        context?.applicationContext?.packageManager?.getPackageInfo(
+            context?.applicationContext?.packageName,
+            0
+        )?.versionName ?: "none"
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        prefRateReview.onPreferenceClickListener = this
+        prefFeedback.onPreferenceClickListener = this
+        prefDevelopers.onPreferenceClickListener = this
+        prefVersion.summary = appVersion
+    }
+
+    override fun onPreferenceClick(preference: Preference?) = when (preference?.key) {
+        getString(R.string.key_rating_review) -> goPlayStore()
+        getString(R.string.key_feedback) -> sendFeedback()
+        getString(R.string.key_developers) -> showDevelopers()
+        else -> false
+    }
+
+    private fun sendFeedback(): Boolean {
+        val emailIntent = ShareCompat.IntentBuilder.from(activity)
+            .setType("plain/text")
+            .setEmailTo(listOf(getString(R.string.email)).toTypedArray())
+            .setSubject(
+                getString(
+                    R.string.send_feedback_email_subject,
+                    getString(R.string.app_name),
+                    appVersion
+                )
+            )
+            .setText(getString(R.string.send_feedback_email_details))
+            .intent
+
+        startActivity(emailIntent)
+        return true
+    }
+
+    private fun goPlayStore(): Boolean {
+        val pkgName = context?.packageName
+        try {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$pkgName")))
+        } catch (t: android.content.ActivityNotFoundException) {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=$pkgName")
+                )
+            )
+        }
+        return true
+    }
+
+    private fun showDevelopers(): Boolean {
+        findNavController().navigate(SettingsFragmentDirections.actionSettingsFragmentToDevelopersFragment())
+        return true
+    }
 
 }
