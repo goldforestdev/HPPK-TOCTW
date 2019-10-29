d3101d03c5b07f4a038a566e0d7c4055c662852f
hyeongju-kim <kinghj.kim@gmail.com>
2019-10-29 23:52:28 +0900

    Booth data class 정의

--- app/src/main/java/com/hppk/toctw/data/model/Booth.kt
+++ app/src/main/java/com/hppk/toctw/data/model/Booth.kt
@@ -2,12 +2,23 @@ package com.hppk.toctw.data.model
 
 data class Booth (
     val id: String = "",
-    val booth: String = "",
+    val title: String = "",
     val location: String = "",
-    val detail: String = "",
+    val description: String = "",
+    val members : List<Staff> = mutableListOf(),
+    val category : List<Category> = mutableListOf(),
     val busy : Busy = Busy.NORMAL
+)
+
+data class Staff (
+    val photoUrl : String = "",
+    val name : String = ""
 )
 
 enum class Busy {
     VERY_BUSY, BUSY, NORMAL
+}
+
+enum class Category {
+    INFO, EVENT, EXPERIENCE
 }
