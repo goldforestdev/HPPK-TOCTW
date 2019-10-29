8c050292395b4dd8ae675b067ef4421818d41a30
YJZZING <yjzzing@gmail.com>
2019-10-29 18:46:53 +0900

    [UI] Home & Main Toolbar

--- app/src/main/java/com/hppk/toctw/ui/MainActivity.kt
+++ app/src/main/java/com/hppk/toctw/ui/MainActivity.kt
@@ -2,26 +2,26 @@ package com.hppk.toctw.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.hppk.toctw.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private val navController: NavController by lazy { findNavController(R.id.nav_host_fragment) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupNavigationMenu()
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val appBarConfiguration = AppBarConfiguration(
+        appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.homeFragment,
                 R.id.schedulesFragment,
@@ -29,11 +29,26 @@ class MainActivity : AppCompatActivity() {
                 R.id.settingsFragment
             ), drawerLayout
         )
-        return navController.navigateUp(appBarConfiguration)
+        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(
+            navController,
+            appBarConfiguration
+        )
+        findViewById<NavigationView>(R.id.navView).setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     private fun setupNavigationMenu() {
         navView.setupWithNavController(navController)
     }
 
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawers()
+        } else {
+            super.onBackPressed()
+        }
+    }
 }
--- app/src/main/java/com/hppk/toctw/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hppk/toctw/ui/home/HomeFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.hppk.toctw.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
@@ -21,5 +23,10 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tv_more_booth.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_schedulesFragment)
+        }
+    }
 }
