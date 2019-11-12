797d58639a4be9072fe8876a82a39972371893ae
KimYoonho Aaron <youknow.yoonho@gmail.com>
2019-11-12 22:16:44 +0900

    FIX - Stamp를 찍은 Child 삭제 안되는 문제

--- app/src/main/java/com/hppk/toctw/data/repository/ChildrenRepository.kt
+++ app/src/main/java/com/hppk/toctw/data/repository/ChildrenRepository.kt
@@ -1,15 +1,22 @@
 package com.hppk.toctw.data.repository
 
 import com.hppk.toctw.data.model.Child
+import com.hppk.toctw.data.model.ChildStampJoin
 import com.hppk.toctw.data.source.ChildrenDao
+import com.hppk.toctw.data.source.local.LocalChildStampDao
+import io.reactivex.Completable
 
 class ChildrenRepository (
     private val localChildrenDao: ChildrenDao,
+    private val localChildStampDao: LocalChildStampDao,
     private val remoteChildrenDao: ChildrenDao? = null
 ) {
     fun save(child: Child) = localChildrenDao.save(child)
 
     fun getAll() = localChildrenDao.getAll()
 
-    fun delete(child: Child) = localChildrenDao.delete(child)
+    fun delete(child: Child): Completable {
+        return localChildStampDao.delete(child.name)
+            .concatWith(localChildrenDao.delete(child))
+    }
 }
